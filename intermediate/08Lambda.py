da4b76bc1bd03b0bc0a531c6286c4a99739234ae
zaennnns <aszaenal618@gmail.com>
2022-07-29 14:54:09 +0700

    complete lambda

---
+++ intermediate/08Lambda.py
@@ -0,0 +1,60 @@
+# add10=lambda x:x+10
+# print(add10(10))
+
+# mult=lambda x,y:x*y
+
+# print(mult(2,7))
+
+
+
+# points2d=[(1,2),(15,1),(5,-1),(10,4)]
+
+# # points2d_sorted = sorted(points2d)
+# # points2d_sorted = sorted(points2d, key=lambda x:x[1])
+# #points2d_sorted = sorted(points2d, key=lambda x:x[0]+x[1])
+
+# print(points2d)
+# print(points2d_sorted)
+
+
+
+
+
+
+
+
+# map(func,seq)
+
+# a=[1,2,3,4,5]
+# b=list(map(lambda x:x+2, a))
+# print(b)
+
+# # list comprehension
+# c=[x+2 for x in a]
+# print(c)
+
+
+
+# filter
+
+
+# a=[1,2,3,4,5]
+# b=list(filter(lambda x:x>2, a))
+# print(b)
+
+# # list comprehension
+# c=[x for x in a if x>2]
+# print(c)
+
+
+# reduce
+
+# from functools import reduce
+
+# a=[1,2,3,4,5,6]
+
+
+# prod_a=reduce(lambda x,y:x+y,a)
+# print(prod_a)
+
+
